735e62011e3bc57272a84c306d26c034b2084082
Jordan Yono <jordanyono@gmail.com>
2021-01-29 21:50:27 -0500

    actually making this run

--- assignment-1/part1.cpp
+++ assignment-1/part1.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
-//#include "mex.hpp" Not sure i even need this
 #include "mexAdapter.hpp"
-
-//using namespace matlab::data;
-//using matlab::mex::ArgumentList;
 
 class MexFunction : public matlab::mex::Function {
     void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) override {
         // Get instance of Matlab Engine API. Equivalent to the engine API's startEngine();
         const std::shared_ptr<matlab::engine::MATLABEngine> &matlabPtr = getEngine();
+
         // Matlab Array factory
         matlab::data::ArrayFactory factory;
 
@@ -17,22 +14,12 @@ class MexFunction : public matlab::mex::Function {
         std::cout << "Argument Check Complete" << "\n";
 
         // Implement function
-        std::cout << "<< 1" << "\n";
-        matlab::data::TypedArray<double> inputValues(std::move(inputs[0]));
-
-
-        std::initializer_list<double> data();
-        for(double elem : inputValues){
-
-        }
-
-        std::vector<matlab::data::Array> args({factory.createArray<double>({inputs[0].getDimensions()}, data)});
-//        std::vector<matlab::data::Array> args({factory.createArray<double>(inputs[0].getDimensions(), inputValues)});
+        std::vector<matlab::data::Array> args(inputs.begin(), inputs.end());
         matlab::data::Array result;
 
-        std::cout << "<< 2" << "\n";
-        result = matlabPtr->feval(std::string("sqrt"), args);
-        std::cout << "<< 3" << "\n";
+        // Call matlab engine sqrt function
+        result = matlabPtr->feval(u"sqrt", args);
+
         // Assign outputs
         outputs[0] = result;
     }
